75782f2f8aa2b3c3c195d79eb3e9028beeb367f2
Wes Belt <wbelt@checkpoint.com>
2018-11-30 11:19:34 -0500

    Add R77 export script with instructions

---
+++ R77-export/README.md
@@ -0,0 +1,58 @@
+# wildcard-object
+Check Point management script to export R77 wildcard objects
+
+## Usage
+```
+usage: wildcard-object.py [-h] {export,update} infile outfile
+
+Wildcard object script v1.0
+
+positional arguments:
+  {export,update}  Operation to perform
+  infile           Objects file with records to convert
+  outfile          Output file
+```
+## Instructions
+
+### Script setup
+1. copy file wildcard-object.py to /home/admin/ on R77.30 management server
+2. ssh into R77.30 management server
+3. enter expert mode
+4. chmod the script to be executable
+```
+chmod u+x /home/admin/wildcard-object.py
+```
+5. edit the whitelist exceptions list on Gaia
+```
+vi /etc/fw/conf/whitelist
+```
+6. Add the following line to the whitelist
+```
+p;/home/admin/wildcard-object.py
+```
+
+### Capture wildcard objects
+
+1. export wildcard objects from objects_5_0.C file to a csv file for use later on R80.20 management server
+```
+/home/admin/wildcard-object.py export /var/opt/CPsuite-R77/fw1/conf/objects_5_0.C /home/admin/output.csv
+```
+2. convert wildcard objects from objects_5_0.C file to have a netmask of 255.255.255.0 so migrate export/import will work properly
+```
+/home/admin/wildcard-object.py convert /var/opt/CPsuite-R77/fw1/conf/objects_5_0.C /home/admin/objects_5_0.C
+```
+3. chmod new objects_5_0.C file to have same permissions as original objects_5_0.C file
+```
+chmod --reference=/var/opt/CPsuite-R77/fw1/conf/objects_5_0.C /home/admin/objects_5_0.C
+```
+
+### Before running migrate export
+
+1. move the original objects_5_0.C file to .original
+```
+mv /var/opt/CPsuite-R77/fw1/conf/objects_5_0.C /var/opt/CPsuite-R77/fw1/conf/objects_5_0.C.original
+```
+2. move upgraded objects_5_0.C file to be active objects file
+```
+mv /home/admin/objects_5_0.C /var/opt/CPsuite-R77/fw1/conf/objects_5_0.C
+```
---
+++ R77-export/wildcard-object.py
@@ -0,0 +1,55 @@
+#!/opt/CPsuite-R77/fw1/Python/bin/python
+# -*- coding: utf-8 -*-
+import re, os, argparse
+
+__version__ = "1.0"
+parser = argparse.ArgumentParser(description="Wildcard object script v" + __version__)
+print ("*** ")
+print ("*** " + parser.description)
+print ("*** ")
+parser.add_argument('mode', choices=["export","update"], help="Operation to perform")
+parser.add_argument("infile", help="Objects file with records to convert")
+parser.add_argument("outfile", help="Output file")
+args = parser.parse_args()
+if args.mode.lower() == "export":
+    if os.path.exists(args.infile):
+        print "objects input file path: " + args.infile
+        with open(args.infile, 'rb') as fi:
+            objectsFile = fi.read()
+        print "file found! file size: {:,} bytes".format(len(objectsFile))
+        findpattern = r':name\s+\((.*)\)(?:[\s\S]{0,250}):color\s+\((.*)\)(?:[\s\S]{0,100}):comments\s+\((.*)\)(?:[\s\S]{0,100}):ipaddr\s+\((.*)\)(?:[\s\S]{0,150}):netmask\s+\((.*)\)(?:[\s\S]{0,100}):use_as_wildcard_netmask\s+\(true\)'
+        matches = re.findall(findpattern,objectsFile)
+        if matches:
+            print "Regex matches: {:,}".format(len(matches))
+            with open(args.outfile, "wb") as fo:
+                #Write CSV header
+                fo.write("name,color,comments,ipv4-address,ipv4-mask-wildcard\n")
+                for match in matches:
+                    fo.write("{},{},{},{},{}\n".format(match[0],match[1],match[2],match[3], match[4]))
+            print ""
+            print "Output written to: {}".format(args.outfile)
+        else:
+            print "No matches found!"
+    else:
+        print "ERROR: File '{}' not found".format(args.infile)
+elif args.mode.lower() == "update":
+    if args.infile:
+        findpattern = r':netmask\s+\(.*\)([\s\S]{0,150}):use_as_wildcard_netmask\s+\(true\)'
+        replpattern = r':netmask (255.255.255.0)\1:use_as_wildcard_netmask (true)'
+        if os.path.exists(args.infile):
+            print "objects input file path: " + args.infile
+            with open(args.infile, 'rb') as fi:
+                objectsFile = fi.read()
+            print "file found! file size: {:,} bytes".format(len(objectsFile))
+            replaced = re.subn(findpattern,replpattern,objectsFile)
+            if replaced[1] > 0:
+                print "replaced: {:,} instances".format(replaced[1])
+                with open(args.outfile, "wb") as fo:
+                    fo.write(replaced[0])
+                print "Conversion complete!"
+                print ""
+                print "Output written to: {}".format(args.outfile)
+            else:
+                print "No matching instances found."
+        else:
+            print "Could not open input file '{}'".format(args.infile)
